2564dd75bf6baf3b1ace121023e5013c0e5b5a99
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2020-01-05 03:15:27 -0300

    Correcoes na autenticacao

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -2,7 +2,8 @@
     <v-container fluid fill-height>
         <v-layout row wrap align-center justify-center style="margin-bottom: 30vh">
             <v-flex xs8 sm4 class="text-xs-center">
-                <img src="../../assets/logo-pro-saude.png" width="300px" class="mb-5 mt-5">
+                <img src="https://firebasestorage.googleapis.com/v0/b/prosaude-36f66.appspot.com/o/assets%2Flogo.svg?alt=media&token=280c2f44-d4e0-4213-b480-30a995c5a254"
+                     width="300px" class="mb-5 mt-5">
                 <v-card style="min-width: 300px" class="mb-5">
                     <v-card-text>
                         <v-text-field
@@ -77,7 +78,7 @@
                     this.loading = true
                     let user = await firebase.auth().signInWithEmailAndPassword(this.email, this.password)
                     await this.$store.dispatch('getUser', user)
-                    this.$router.push('/')
+                    await this.$router.push('/')
                 } catch (e) {
                     switch (e.code) {
                         case 'auth/user-not-found':
