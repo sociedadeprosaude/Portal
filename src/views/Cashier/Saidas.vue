0b5ef721e878bb4c8a0d10f420a3b8ea0a185fd0
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2020-01-18 11:18:06 -0300

    conserto de erro de grafia e falta de colaborador em outtakes

--- src/views/Cashier/Saidas.vue
+++ src/views/Cashier/Saidas.vue
@@ -13,58 +13,60 @@
         </v-layout>
         <v-layout row wrap>
             <v-content>
-            <v-flex xs12>
-                <v-card>
-                    <v-layout wrap>
-                        <v-flex xs5>
-                            <v-select
-                                    label="categoria"
-                                    v-model="categoria"
-                                    :items="Categorias"
-                            ></v-select>
-                        </v-flex>
-                        <v-flex xs2>
-                            <v-tooltip bottom>
-                                <template v-slot:activator="{ on }">
-                                    <v-btn class="mx-2" fab dark small color="blue" @click="() => {verificador = !verificador}">
-                                        <v-icon color="primary" v-on="on">add</v-icon>
-                                    </v-btn>
-                                </template>
-                                <span>Adicionar nova Categoria</span>
-                            </v-tooltip>
-                        </v-flex>
-                        <v-spacer></v-spacer>
-                        <v-flex xs4>
-                            <v-currency-field
-                                    label="valor"
-                                    v-model="valor"
-                                    :rules="[rules.required||rules.valor]"
-                                    required
-                            ></v-currency-field>
-                        </v-flex>
-                        <v-flex xs12>
-                            <v-text-field
-                                    label="Descrição"
-                                    v-model="descricao"
-                            >
-                            </v-text-field>
-                        </v-flex>
-                        <v-flex rigth>
-                            <submit-button text="Enviar" :loading="loading" :success="success" @reset="success = false"
-                                           @click="enviar()">
-                            </submit-button>
-                        </v-flex>
-                    </v-layout>
-                </v-card>
-            </v-flex>
+                <v-flex xs12>
+                    <v-card>
+                        <v-layout wrap>
+                            <v-flex xs5>
+                                <v-select
+                                        label="categoria"
+                                        v-model="categoria"
+                                        :items="Categorias"
+                                ></v-select>
+                            </v-flex>
+                            <v-flex xs2>
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn class="mx-2" fab dark small color="blue"
+                                               @click="() => {verificador = !verificador}">
+                                            <v-icon color="primary" v-on="on">add</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span>Adicionar nova Categoria</span>
+                                </v-tooltip>
+                            </v-flex>
+                            <v-spacer></v-spacer>
+                            <v-flex xs4>
+                                <v-currency-field
+                                        label="valor"
+                                        v-model="valor"
+                                        :rules="[rules.required||rules.valor]"
+                                        required
+                                ></v-currency-field>
+                            </v-flex>
+                            <v-flex xs12>
+                                <v-text-field
+                                        label="Descrição"
+                                        v-model="descricao"
+                                >
+                                </v-text-field>
+                            </v-flex>
+                            <v-flex rigth>
+                                <submit-button text="Enviar" :loading="loading" :success="success"
+                                               @reset="success = false"
+                                               @click="enviar()">
+                                </submit-button>
+                            </v-flex>
+                        </v-layout>
+                    </v-card>
+                </v-flex>
             </v-content>
             <v-dialog
-             v-model="verificador"
+                    v-model="verificador"
             >
                 <v-card>
                     Adicionar Nova Categoria
                     <v-text-field
-                    v-model="categoriaNova"
+                            v-model="categoriaNova"
                     >
                     </v-text-field>
                     <submit-button text="Adicionar" :loading="loading" :success="success" @reset="success = false"
@@ -82,39 +84,47 @@
     import SubmitButton from "../../components/SubmitButton";
 
 
-
     export default {
         components: {SubmitButton},
         data: () => ({
-            descricao:'',
-            categoria:'',
-            valor:0,
-            verificador:false,
-            categoriaNova:'',
-            aviso:false,
+            descricao: '',
+            categoria: '',
+            valor: 0,
+            verificador: false,
+            categoriaNova: '',
+            aviso: false,
             now: moment().valueOf(),
             loading: false,
             success: false,
             data: moment().format("YYYY-MM-DD HH:mm:ss"),
             rules: {
-                valor: value =>{
+                valor: value => {
                     const pattern = /(^(\d)+(,(\d)+)*$|^$)/
                     return pattern.test(value) || 'Digite apenas números exemplo:" 2.5 "'
                 }
             }
         }),
         methods: {
-            enviar(){
+            enviar() {
                 this.loading = true;
-                this.$store.dispatch('AddSaida',{descricao:this.descricao, category:this.categoria, value: parseFloat(this.valor), id:moment().valueOf(), date:this.data, paid:this.data, payment_method:'Dinheiro'})
+                this.$store.dispatch('AddSaida', {
+                    description: this.descricao,
+                    category: this.categoria,
+                    value: parseFloat(this.valor),
+                    id: moment().valueOf(),
+                    date: this.data,
+                    paid: this.data,
+                    colaborator: this.user,
+                    payment_method: 'Dinheiro'
+                })
                 this.success = true;
                 this.loading = false;
-                this.descricao='';
-                this.valor=0;
-                this.categoria='';
+                this.descricao = '';
+                this.valor = 0;
+                this.categoria = '';
             },
-            adicionarCategoria(){
-                this.$store.dispatch('AddCategorie',{categoria: this.categoriaNova})
+            adicionarCategoria() {
+                this.$store.dispatch('AddCategorie', {categoria: this.categoriaNova})
                 this.success = true;
                 this.loading = false;
                 this.$store.dispatch('LoadCategories');
@@ -122,10 +132,10 @@
                     this.categoriaNova = '';
                     this.verificador = false;
                 }, 1000)
-                this.verificador= !this.verificador;
-                this.categoriaNova= '';
+                this.verificador = !this.verificador;
+                this.categoriaNova = '';
             },
-            back(){
+            back() {
                 this.$router.push('/caixa')
             },
         },
@@ -133,9 +143,12 @@
             this.$store.dispatch('LoadCategories')
         },
         computed: {
-         Categorias(){
-             return this.$store.getters.categoria;
-         }
+            Categorias() {
+                return this.$store.getters.categoria;
+            },
+            user() {
+                return this.$store.getters.user
+            }
         }
 
     }
