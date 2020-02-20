831633f402e20ec4a032a7d508b683da349f7b68
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2020-02-20 15:04:20 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -75,7 +75,7 @@
                 </v-layout>
             </v-flex>
             <v-flex xs12 class="text-right">
-                <span>1.3.4</span>
+                <span>1.4</span>
             </v-flex>
         </v-layout>
         <v-layout row wrap v-else class="align-center">
