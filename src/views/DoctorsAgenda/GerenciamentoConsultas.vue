cd8bfa4f2511b38eaf37ce40dc6de042c840523c
JonatasRodriguesReis <40697229+JonatasRodriguesReis@users.noreply.github.com>
2020-04-08 01:07:38 -0400

    bug fix agendamento
    Não estava certo o contador do numero de vagas disponiveis

--- src/views/DoctorsAgenda/GerenciamentoConsultas.vue
+++ src/views/DoctorsAgenda/GerenciamentoConsultas.vue
@@ -550,6 +550,8 @@
                 let consultas = this.$store.getters.consultations.filter((a) => {
                     return this.especialidade && this.date ? this.especialidade.name === a.specialty.name && this.date === a.date.split(' ')[0] && a.user : false
                 })
+
+                console.log('Geren',consultas)
                 return consultas;
             },
             /* menssagens:{
