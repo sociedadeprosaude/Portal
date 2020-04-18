a7c729df744777b1cba7fa455be4cb69bf1133f3
Adria <adria.acastro@gmail.com>
2020-04-17 23:51:39 -0400

    Revert "update bill"
    This reverts commit 94d578f938d7ff6e1fec4899f2f039f94b2fcbb2.

--- src/views/Bills.vue
+++ src/views/Bills.vue
@@ -476,11 +476,6 @@ export default {
       }
       await this.$store.dispatch("addOuttakes", bill);
       await this.$store.dispatch("getOuttakes");
-      await this.$store.dispatch("getOuttakesPending", {
-        finalDate: moment()
-                .add(5, "days")
-                .format("YYYY-MM-DD 23:59:59")
-      });
       this.loading = false;
     },
     async unpayOuttake(outtake) {
