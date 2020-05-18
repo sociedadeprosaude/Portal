cad80ee67a6f78c2238ae4f7678b0b219e4ac8f6
RaulHenriqueM <rhmo.eng17@uea.edu.br>
2020-05-18 16:21:19 -0400

    .

--- src/components/doctorsAgenda/DataToSearchConsultation.vue
+++ src/components/doctorsAgenda/DataToSearchConsultation.vue
@@ -33,7 +33,9 @@
         }),
 
         async mounted()  {
-            await this.$store.dispatch("getSchedules", {
+             await this.$store.dispatch(
+                "getSchedules",
+                {
                     start_date: moment()
                         .subtract(5, "hours")
                         .format("YYYY-MM-DD HH:mm:ss"),
@@ -88,6 +90,22 @@
                     easing: "easeInQuint",
                 };
             },
+            schedules() {
+                let schedules = this.$store.getters.schedules.filter(a => {
+                    let response = true;
+                    if (this.doctor) {
+                        if (this.doctor.cpf !== a.doctor.cpf) response = false;
+                    }
+                    if (this.specialty) {
+                        if (this.specialty.name !== a.specialty.name) response = false;
+                    }
+                    if (this.clinic) {
+                        if (this.clinic !== a.clinic.name) response = false;
+                    }
+                    return response;
+                });
+                return this.consultationsOfSchedules(schedules);
+            },
 
             consultations () {
                 return this.$store.getters.consultations.filter(a => {
@@ -112,12 +130,14 @@
 
             allowedDates(val) {
                 return (
-                    console.log(Object.keys(this.consultationsDateFilter(this.consultations)).indexOf(val))
+                    Object.keys(this.consultationsDateFilter(this.consultations)).indexOf(val) !== -1
                 );
             },
 
             consultationsDateFilter (consultations) {
-                
+                console.log('consultation : ', consultations)
+
+
                 let res = {};
                 consultations.sort((a,b)=>{
                     return a.date > b.date ? 1 : a.date < b.date ? -1 : 0
@@ -129,8 +149,50 @@
                     }
                     res[targetDate].push(consultations[cons]);
                 }
+                console.log('res: ', res)
                 return res;
-            }
+            },
+            numberVacancyAndReturns(schedule) {
+                let consultations = this.consultations;
+                return consultations.reduce((obj,item)=>{
+
+                    if (schedule.clinic.name === item.clinic.name && schedule.specialty.name === item.specialty.name
+                        && schedule.doctor.cpf === item.doctor.cpf && schedule.date === item.date  && item.user){
+                        if (item.type === 'Consulta') {
+                            obj.qtd_consultations = obj.qtd_consultations +  1
+                        } else
+                            obj.qtd_returns += 1
+                    }
+                    return obj
+                },{qtd_consultations:0,qtd_returns:0})
+            },
+            consultationsOfSchedules(schedules){
+                let consultations = [];
+                schedules.forEach((schedule)=>{
+                    let keys = Object.keys(schedule.days);
+                    let dates = this.datesOfInterval({weekDays:keys});
+
+                    dates.forEach((date)=>{
+                        let hourConsultation = schedule.days[moment(date).weekday()].hour;
+                        if(schedule.cancelations_schedules.indexOf(date) === -1 && schedule.cancelations_schedules.indexOf(date + ' ' +hourConsultation) === -1){
+                            let scheduleObj = {
+                                clinic: schedule.clinic,
+                                doctor: schedule.doctor,
+                                date: date + ' ' + hourConsultation,
+                                routine_id : schedule.routine_id,
+                                specialty: schedule.specialty,
+                                vacancy: schedule.days[moment(date).weekday()].vacancy,
+                                id_schedule: schedule.id,
+
+                            };
+                            let obj = {...scheduleObj,...this.numberVacancyAndReturns(scheduleObj)}
+                            obj.vacancy = obj.vacancy - obj.qtd_consultations - obj.qtd_returns
+                            consultations.push(obj)
+                        }
+                    })
+                });
+                return consultations
+            },
 
 
         }
