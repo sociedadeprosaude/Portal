67df4f677ac825dec254c5ce3816197df85f16de
Adria Albuquerque <adria.acastro@gmail.com>
2020-06-02 19:36:16 -0400

    Update

--- src/views/Operational/DoctorsAgenda/DoctorScheduling.vue
+++ src/views/Operational/DoctorsAgenda/DoctorScheduling.vue
@@ -1,15 +1,28 @@
 <template>
     <v-container fluid class="ma-0 pa-0">
         <v-layout row wrap>
-            <v-flex sm3 lg3 class="primary">
+            <v-flex sm3 class="primary hidden-md-and-down">
                 <DataToSearchConsultation @GetConsultations="schedules= $event" :daysToListen="daysToListen"/>
             </v-flex>
-            <v-flex sm6 lg6>
+            <v-flex sm6 class="hidden-md-and-down">
                 <SchedulesConsultations :Consultations="schedules" @refreshDate="daysToListen = $event"/>
             </v-flex>
-            <v-flex sm3 lg3 class="primary">
+            <v-flex sm3 class="primary hidden-md-and-down">
                 <DataCashierConsultation/>
             </v-flex>
+            <v-flex xs12 class="hidden-md-and-up" >
+                <v-layout row wrap class="justify-space-between primary">
+                    <v-btn v-for="item in items" :key="item.title" class="primary" text large @click="chooseView(item.view)">
+                        {{item.title}}
+                    </v-btn>
+                </v-layout>
+            </v-flex>
+            <v-flex xs12 class="hidden-md-and-up">
+                <DataToSearchConsultation @GetConsultations="schedules= $event" :daysToListen="daysToListen" v-if="this.viewSeleted === 'data'"/>
+                <SchedulesConsultations :Consultations="schedules" @refreshDate="daysToListen = $event" v-if="this.viewSeleted === 'schedules'"/>
+                <DataCashierConsultation v-if="this.viewSeleted === 'cashier'"/>
+            </v-flex>
+
         </v-layout>
     </v-container>
 </template>
@@ -24,7 +37,19 @@
         data: () => ({
             schedules: {},
             daysToListen: null,
+            viewSeleted: 'data',
+            items: [
+                {title: 'Dados', view: 'data'},
+                {title: 'Consultas', view: 'schedules'},
+                {title: 'Caixa', view: 'cashier'}
+            ]
         }),
+
+        methods: {
+            chooseView(view) {
+                this.viewSeleted = view;
+            }
+        }
 
     }
 </script>
