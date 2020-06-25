e89ba4af3ef82db9e46be4929b99542383f9cded
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2020-06-24 23:08:08 -0300

    sector ticket

--- src/views/Operational/Tickets/RoomDashboard.vue
+++ src/views/Operational/Tickets/RoomDashboard.vue
@@ -4,7 +4,7 @@
       <v-col sm="12" md="4">
         <v-tooltip top v-if="doctorsLoaded">
           <template v-slot:activator="{ on }">
-            <v-btn width="100%" v-on="on" class="primary" rounded @click="skipNextTicket">
+            <v-btn width="100%" v-on="on" class="primary" rounded @click="generateSectorTicket">
               Proxima senha:
               {{ticketInfo.ticket_number}}
             </v-btn>
@@ -35,7 +35,7 @@
             <v-row cols="12">
               <v-text-field v-model="room.name" label="Nome da Sala"></v-text-field>
             </v-row>
-         
+
             <v-row>
               <v-col cols="12" class="text-right">
                 <submit-button
@@ -281,131 +281,144 @@ export default {
     rooms() {
       return this.sector ? this.sector.rooms : [];
     },
+            roomsLoaded() {
+                return this.$store.getters.roomsLoaded;
+            },
+            ticketInfo() {
+                if (
+                    moment(this.$store.getters.ticketGeneralInfo.last_updated).dayOfYear !==
+                    moment().dayOfYear
+                ) {
+                    this.reset();
+                }
+                return this.$store.getters.ticketGeneralInfo;
+            },
+            doctors() {
+                return Object.values(this.$store.getters.doctors).filter(doctor => {
+                    return doctor.name.includes(
+                        this.doctorsListDialog.search.toUpperCase()
+                    );
+                });
+            },
+            doctorsLoaded() {
+                return this.$store.getters.doctorsLoaded;
+            },
+            sectorName() {
+                return this.$route.params['sector_name']
+            },
+            sector() {
+                return this.$store.getters.sectors ? this.$store.getters.sectors.find((sector) => sector.name == this.sectorName) : undefined
+            }
+        },
+        methods: {
+            async initialInfo() {
+                if (!this.sector) {
+                    this.$store.dispatch("getTicketsGeneralInfo");
+                    this.$store.dispatch('listenTicketsSectors')
+                }
+            },
+            getActualTicket(tickets) {
+                let calledTickets = tickets.filter(ticket => {
+                    return ticket.called_at;
+                });
+                if (calledTickets.length === 0) {
+                    return undefined;
+                }
+                return calledTickets[calledTickets.length - 1];
+            },
+            async createRoom(room) {
+                this.loading = true;
+                const sector = this.sector;
+                await this.$store.dispatch("createSectorRoom", {sector, room});
+                this.loading = false;
+                this.success = true;
+                setTimeout(() => {
+                    this.success = false;
+                }, 1000);
+            },
+            async setDoctorToRoom(room, doctor) {
+                room.doctor = doctor;
+                this.loading = true;
+                const sector = this.sector;
+                await this.$store.dispatch("updateSectorRoom", {sector, room});
+                this.loading = false;
+            },
+            async generateNextTicket(room) {
+                if (!room.tickets) {
+                    room.tickets = [];
+                }
+                room.tickets.push({
+                    number: this.ticketInfo.ticket_number,
+                    created_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+                    doctor: room.doctor
+                });
+                await this.upgradeTicketNumber()
+                const sector = this.sector;
+                await this.$store.dispatch("updateSectorRoom", {sector, room});
+            },
+            async generateSectorTicket() {
+                console.log(this.ticketInfo)
+                let sector = this.sector
+                if (!sector.tickets) {
+                    sector.tickets = []
+                }
+                sector.tickets.push({
+                    number: this.ticketInfo.ticket_number,
+                    created_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+                })
+                await this.upgradeTicketNumber()
+                await this.$store.dispatch('updateSector', sector)
+            },
+            async upgradeTicketNumber() {
+                let ticketInfo = this.ticketInfo;
+                ticketInfo.ticket_number++;
+                ticketInfo.last_updated = moment().format("YYYY-MM-DD HH:mm:ss");
+                await this.$store.dispatch("updateGeneralInfo", this.ticketInfo);
+            },
+            async callNextTicket(room) {
+                let ticketIndex = room.tickets.findIndex(ticket => {
+                    return !ticket.called_at;
+                })
+                if (ticketIndex < 0) {
+                    this.callSectorTicket(room)
+                    return
+                }
 
-    roomsLoaded() {
-      return this.$store.getters.roomsLoaded;
-    },
-    ticketInfo() {
-      if (
-        moment(this.$store.getters.ticketGeneralInfo.last_updated).dayOfYear !==
-        moment().dayOfYear
-      ) {
-        this.reset();
-      }
-      return this.$store.getters.ticketGeneralInfo;
-    },
-    doctors() {
-      return Object.values(this.$store.getters.doctors).filter(doctor => {
-        return doctor.name.includes(
-          this.doctorsListDialog.search.toUpperCase()
-        );
-      });
-    },
-    doctorsLoaded() {
-      return this.$store.getters.doctorsLoaded;
-    },
-    sectorName() {
-      return this.$route.params["sector_name"];
-    },
-    sector() {
-      return this.$store.getters.sectors
-        ? this.$store.getters.sectors.find(
-            sector => sector.name == this.sectorName
-          )
-        : undefined;
-    }
-  },
-  methods: {
-    async initialInfo() {
-      if (!this.sector) {
-        this.$store.dispatch("getTicketsGeneralInfo");
-        this.$store.dispatch("listenTicketsSectors");
-      }
-    },
-    getActualTicket(tickets) {
-      let calledTickets = tickets.filter(ticket => {
-        return ticket.called_at;
-      });
-      if (calledTickets.length === 0) {
-        return undefined;
-      }
-      return calledTickets[calledTickets.length - 1];
-    },
-    async createRoom(room) {
-      this.loading = true;
-      const sector = this.sector;
-      await this.$store.dispatch("createSectorRoom", { sector, room });
-      this.loading = false;
-      this.success = true;
-      setTimeout(() => {
-        this.success = false;
-      }, 1000);
-    },
-    async setDoctorToRoom(room, doctor) {
-      room.doctor = doctor;
-      this.loading = true;
-      const sector = this.sector;
-      await this.$store.dispatch("updateSectorRoom", { sector, room });
-      this.loading = false;
-    },
-    async generateNextTicket(room) {
-      let ticketInfo = this.ticketInfo;
-      if (!room.tickets) {
-        room.tickets = [];
-      }
-      room.tickets.push({
-        number: this.ticketInfo.ticket_number,
-        created_at: moment().format("YYYY-MM-DD HH:mm:ss"),
-        doctor: room.doctor
-      });
-      ticketInfo.ticket_number++;
-      ticketInfo.last_updated = moment().format("YYYY-MM-DD HH:mm:ss");
-      await this.$store.dispatch("updateGeneralInfo", this.ticketInfo);
-      const sector = this.sector;
-      await this.$store.dispatch("updateSectorRoom", { sector, room });
-    },
-    async callNextTicket(room) {
-      let ticketIndex = room.tickets.findIndex(ticket => {
-        return !ticket.called_at;
-      });
-      if (ticketIndex < 0) return;
+                room.tickets[ticketIndex].called_at = moment().format(
+                    "YYYY-MM-DD HH:mm:ss"
+                );
+                const sector = this.sector;
+                await this.$store.dispatch("updateSectorRoom", {sector, room});
+            },
+            async callSectorTicket(room) {
+                let ticketIndex = this.sector.tickets.findIndex(ticket => {
+                    return !ticket.called_at;
+                })
+                if (ticketIndex < 0) {
+                    return
+                }
+                this.sector.tickets[ticketIndex].called_at = moment().format(
+                    "YYYY-MM-DD HH:mm:ss"
+                );
+                room.tickets.push(this.sector.tickets[ticketIndex])
+                const sector = this.sector;
+                await this.$store.dispatch("updateSectorRoom", {sector, room});
+                this.sector.tickets.splice(ticketIndex, 1)
+                await this.$store.dispatch("updateSector", this.sector);
 
-      room.tickets[ticketIndex].called_at = moment().format(
-        "YYYY-MM-DD HH:mm:ss"
-      );
-      const sector = this.sector;
-      await this.$store.dispatch("updateSectorRoom", { sector, room });
-    },
-    async deleteRoom(room) {
-      this.deletionRoom.selectedRoom = room;
-      if (!this.deletionRoom.deleteRoomDialog) {
-        this.deletionRoom.deleteRoomDialog = true;
-        return;
-      }
-      this.deletionRoom.deleting = true;
-      await this.$store.dispatch("deleteSectorRoom", {
-        room: room,
-        sector: this.sector
-      });
-      this.deletionRoom.deleting = false;
-      this.deletionRoom.deleteRoomDialog = false;
-    },
-    alertActualTicket(room) {},
-    reset() {
-      let ticketInfo = this.ticketInfo;
-      ticketInfo.ticket_number = 1;
-      ticketInfo.last_updated = moment().format("YYYY-MM-DD HH:mm:ss");
-      this.$store.dispatch("updateGeneralInfo", ticketInfo);
-    },
-    skipNextTicket() {
-      let ticketInfo = this.ticketInfo;
-      ticketInfo.ticket_number++;
-      this.$store.dispatch("updateGeneralInfo", ticketInfo);
-    },
-    openSingleView(room) {
-      this.singleViewDialog.room = room;
-      this.singleViewDialog.active = true;
-    }
+            },
+            async deleteRoom(room) {
+                this.deletionRoom.selectedRoom = room
+                if (!this.deletionRoom.deleteRoomDialog) {
+                    this.deletionRoom.deleteRoomDialog = true
+                    return
+                }
+                this.deletionRoom.deleting = true
+                await this.$store.dispatch('deleteSectorRoom', {room: room, sector: this.sector})
+                this.deletionRoom.deleting = false
+                this.deletionRoom.deleteRoomDialog = false
+            },
+            alertActualTicket(room) {}
   }
 };
 </script>
