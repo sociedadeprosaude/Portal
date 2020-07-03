5f5d62bd4f1c7f168181ceb38670afa61fc8cb5f
Adria Albuquerque <adria.acastro@gmail.com>
2020-07-03 11:52:33 -0400

    refresh ultima senha

--- src/store/modules/Tickets.js
+++ src/store/modules/Tickets.js
@@ -183,6 +183,7 @@ const actions = {
             ...payload.sector.rooms,
             [payload.room.name]: payload.room
         };
+        console.log('selectedClinic', selectedClinic);
         await queryBuilder(selectedClinic.name, payload.sector.name)
             .update({
                 rooms: payload.sector.rooms
