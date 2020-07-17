caba5349973e656da29a2c46b3a1cb8185f2752e
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2020-07-17 16:33:29 -0300

    2.5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -94,7 +94,7 @@
                 </v-layout>
             </v-flex>
             <v-flex xs12 class="text-right">
-                <span>2.4</span>
+                <span>2.5</span>
             </v-flex>
         </v-layout>
         <v-layout row wrap v-else class="align-center">
