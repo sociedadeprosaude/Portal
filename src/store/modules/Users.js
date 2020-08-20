fbc76be2c7ce902dc2df1e8b4ce3ad1818a7eac0
seu_usr <jkdsgg@gmail.com>
2020-08-20 17:05:53 -0400

    up

--- src/store/modules/Users.js
+++ src/store/modules/Users.js
@@ -261,7 +261,6 @@ const actions = {
                 patient.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
                 user = await firebase.firestore().collection('users').add(patient)
             }
-            console.log('chave:', user.id)
             return user
         } catch (e) {
             throw e
