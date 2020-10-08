6cfcba06e90d1bfc370dafa9beeebb2c812bcb07
RaulHenriqueM <rhmo.eng17@uea.edu.br>
2020-10-08 09:57:10 -0400

    Selecionando paciente no comprovante

--- src/components/doctorsAgenda/ManagementConsultations/CardInformationManagementConsultations.vue
+++ src/components/doctorsAgenda/ManagementConsultations/CardInformationManagementConsultations.vue
@@ -202,6 +202,8 @@
                 this.consultation.consultation_hour = consultation_hour; */
             },
             ConsultationRecept(consultation) {
+                this.$store.commit('setSelectedPatient', consultation.patient);
+                console.log('consultation: ', consultation)
                 this.receptDialog = true;
             }
         },
